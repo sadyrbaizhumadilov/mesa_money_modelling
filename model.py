f8f8a200faf19f29591df9d5af265daa2adc25ed
Sadyrbai <sadyrbaizhumadilov@gmail.com>
2023-04-13 14:52:00 +0300

    first commit

---
+++ model.py
@@ -0,0 +1,27 @@
+import mesa
+from agent import MoneyAgent
+from mesa.time import RandomActivation
+from mesa.space import MultiGrid
+
+class MoneyModel(mesa.Model):
+    """A model with some number of agents."""
+
+    def __init__(self, N, width, height):
+        self.num_agents = N
+        self.grid = MultiGrid(width, height, True)
+        self.schedule = RandomActivation(self)
+        self.running=True
+
+        # Create agents
+        for i in range(self.num_agents):
+            a = MoneyAgent(i, self)
+            self.schedule.add(a)
+            # Add the agent to a random grid cell
+            x = self.random.randrange(self.grid.width)
+            y = self.random.randrange(self.grid.height)
+            self.grid.place_agent(a, (x, y))
+
+    
+    def step(self):
+        
+        self.schedule.step()
