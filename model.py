8d592974b838915c121f625894d52604e6c9e3e0
Sadyrbai <sadyrbaizhumadilov@gmail.com>
2023-05-14 17:11:57 +0300

    some updates here

--- model.py
+++ model.py
@@ -2,16 +2,23 @@ import mesa
 from agent import MoneyAgent
 from mesa.time import RandomActivation
 from mesa.space import MultiGrid
+from mesa.datacollection import DataCollector
 
 class MoneyModel(mesa.Model):
     """A model with some number of agents."""
 
-    def __init__(self, N, width, height):
-        self.num_agents = N
+    def __init__(self, number_of_agents, width, height):
+        self.num_agents = number_of_agents
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
         self.running=True
+        self.datacollector_currents=DataCollector(
+            {
+               "Wealthy Agents":MoneyModel.current_wealthy_agents,
+               "Non Wealthy Agents":MoneyModel.current_non_wealthy_agents,
+            
 
+        })
         # Create agents
         for i in range(self.num_agents):
             a = MoneyAgent(i, self)
@@ -23,5 +30,11 @@ class MoneyModel(mesa.Model):
 
     
     def step(self):
-        
         self.schedule.step()
+        self.datacollector_currents.collect(self)
+    @staticmethod
+    def current_wealthy_agents(model) -> int:
+      return sum([1 for agent in model.schedule.agents if agent.wealth > 0])
+    @staticmethod
+    def current_non_wealthy_agents(model) -> int:
+      return sum([1 for agent in model.schedule.agents if agent.wealth == 0])
--- server.py
+++ server.py
@@ -1,27 +1,61 @@
 from model import MoneyModel
 import mesa
 from mesa.visualization.ModularVisualization import ModularServer
-from mesa.visualization.modules import CanvasGrid
+from mesa.visualization.modules import CanvasGrid,ChartModule
+from mesa.visualization.UserParam import UserSettableParameter
+
+NUMBER_OF_CELL=30
+SIZE_OF_CANVAS_IN_PIXELS_X=700
+SIZE_OF_CANVAS_IN_PIXELS_Y=500
+simulation_params= {
+    "number_of_agents": UserSettableParameter( 
+    "slider",
+    "Number of Agents",
+    50, #default
+    10, #min 
+    200, #max
+    1.0, #step
+    description="Choose how many agents to include in the simulation",
+    ),
+   
+    "width": NUMBER_OF_CELL,
+    "height": NUMBER_OF_CELL,
+}
+
 
 def agent_portrayal(agent):
     portrayal = {"Shape": "circle",
                  "Filled": "true",
-                 "r": 0.5}
+                 "r": agent.wealth +0.1}
 
-    if agent.wealth > 0:
-        portrayal["Color"] = "red"
+    if agent.wealth > 2:
+        portrayal["Color"] = "green"
         portrayal["Layer"] = 0
+    elif agent.wealth > 1:
+        portrayal["Color"] = "yellow"
+        portrayal["Layer"] = 1
+    elif agent.wealth > 0:
+        portrayal["Color"] = "red"
+        portrayal["Layer"] = 2
     else:
         portrayal["Color"] = "grey"
-        portrayal["Layer"] = 1
-        portrayal["r"] = 0.2
+        portrayal["Layer"] = 3
+        portrayal["r"] = 0.7
     return portrayal
 
-grid = CanvasGrid(agent_portrayal, 10, 10, 500, 500)
+grid = CanvasGrid(agent_portrayal, NUMBER_OF_CELL, NUMBER_OF_CELL, SIZE_OF_CANVAS_IN_PIXELS_X, SIZE_OF_CANVAS_IN_PIXELS_Y)
+
+chart_currents=ChartModule(
+    [
+    {"Label":"Wealthy Agents","Color":"green"},
+     {"Label":"Non Wealthy Agents","Color":"grey"},
+    ],
+    canvas_height=300,
+    data_collector_name="datacollector_currents"
+
+)
 server = ModularServer(MoneyModel,
-                       [grid],
-                       "Money Model",
-                       {"N":100, "width":10, "height":10})
+                       [grid,chart_currents], "Money Model",simulation_params)
 
 server.port = 8521 # The default
 server.launch()
